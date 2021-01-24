4537955d38d336c2098c139b5de99230e697de11
kedges <798236882@qq.com>
2021-01-24 02:35:39 +0000

    Update edgemain.jl

--- edgemain.jl
+++ edgemain.jl
@@ -14,15 +14,18 @@ for i = 1 : n
     #fout = open(string("data/","finnal.ans"),"a")
     #println("Now running file:",str[1],"with eps=",ep);
     for k= 1 : 3
-        println("Now running file:",str[1],"with eps=",ep);
-        fout = open(string("data/","time.ans"),"a")
-        print(fout,str[1]," ");
         ep = 0.1*k;
+        println("Now running file:",str[1],"with eps=",ep);
+        fout = open(string("data/","time.ans"),"a");
+        print(fout,str[1]," ");
         greed = gre(G,G.k ,ep,fout);
         println(fout," ");
+        println("gre=",greed);
         close(fout);
     end
+    fout = open(string("data/","time.ans"),"a");
     exact=exa(G,G.k,fout);
+    println("exa=",exact);
 end
 
 close(fname)
